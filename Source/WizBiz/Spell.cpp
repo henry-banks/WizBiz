cad9b38f47dfb6d06f4509abd663daa43339d067
Shmiddy McShmiddington <mrshmid@gmail.com>
2017-03-20 07:44:30 -0700

    made a spell

---
+++ Source/WizBiz/Spell.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WizBiz.h"
+#include "Spell.h"
+
+
+// Sets default values
+ASpell::ASpell()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASpell::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpell::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
